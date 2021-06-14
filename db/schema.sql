ef1e91bc5c606f37fb60704afd9fe6465e60565f
tom2u <tbrodhead@2u.com>
2021-06-14 13:44:02 -0500

    feature/locations

---
+++ db/schema.sql
@@ -0,0 +1,56 @@
+DROP DATABASE IF EXISTS diving_db;
+
+CREATE DATABASE diving_db;
+
+\c diving_db;
+
+CREATE DOMAIN UNSIGNED AS INTEGER CHECK (VALUE > 0);
+
+CREATE DOMAIN LATLONG AS POINT
+  CHECK (VALUE[0] BETWEEN -90 AND 90)
+  CHECK (VALUE[1] BETWEEN -180 AND 180);
+
+
+CREATE TABLE certifications (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  minimum_age UNSIGNED,
+  required_hours UNSIGNED
+);
+
+CREATE TABLE divers (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  is_instructor BOOLEAN DEFAULT FALSE NOT NULL,
+  certification_id INTEGER REFERENCES certifications(id) ON DELETE SET NULL
+);
+
+CREATE TABLE locations (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  coordinates LATLONG NOT NULL
+);
+
+CREATE TABLE tags (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  location_id INTEGER NOT NULL REFERENCES locations(id) ON DELETE CASCADE,
+  UNIQUE(name, location_id)
+);
+
+CREATE TABLE dives (
+  id SERIAL PRIMARY KEY,
+  depth NUMERIC(5, 2) NOT NULL,
+  dive_date TIMESTAMP NOT NULL DEFAULT NOW(),
+  duration UNSIGNED NOT NULL,
+  diver_id INTEGER NOT NULL REFERENCES divers(id) ON DELETE CASCADE,
+  location_id INTEGER NOT NULL REFERENCES locations(id) ON DELETE CASCADE
+);
+
+CREATE FUNCTION random_between(low INT, high INT)
+RETURNS INT AS $$
+BEGIN
+  RETURN FLOOR(RANDOM() * (high - low + 1) + low);
+END;
+$$ LANGUAGE plpgsql;
---
+++ db/seeds.sql
@@ -0,0 +1,58 @@
+\c diving_db;
+
+BEGIN;
+
+INSERT INTO certifications (name, minimum_age, required_hours)
+VALUES
+  ('Open Water Diver', 10, 15),
+  ('Advanced Open Water Diver', 12, 8),
+  ('Enriched Air Diver', 12, 18),
+  ('Rescue Diver', 12, 12),
+  ('Deep Diver', 15, 4);
+
+INSERT INTO divers (first_name, last_name, is_instructor, certification_id)
+VALUES
+  ('Darya', 'Poole', TRUE, 2),
+  ('Douglas', 'Fisher', FALSE, 3),
+  ('Kai', 'Seabrook', FALSE, 3),
+  ('Kendall', 'Rivers', TRUE, 4),
+  ('Mira', 'Waters', FALSE, 5);
+
+INSERT INTO locations (name, coordinates)
+VALUES
+  ('Barracuda Point', POINT(4.112742, 118.631106)),
+  ('Cape Kri', POINT(-0.556324, 130.690725)),
+  ('SS Yongala', POINT(-19.304703, 147.623977)),
+  ('Great Blue Hole', POINT(17.316007, -87.535225)),
+  ('Richelieu Rock', POINT(9.362752, 98.021727));
+
+INSERT INTO tags (location_id, name)
+VALUES
+  (1, 'wall'),
+  (2, 'sharks'),
+  (2, 'current'),
+  (3, 'wreck'),
+  (3, 'sharks'),
+  (5, 'sharks'),
+  (5, 'reef'),
+  (5, 'current');
+
+INSERT INTO locations (name, coordinates)
+SELECT
+  CONCAT('Unknown Location #', loc_id),
+  POINT(
+    random_between(-90, 90),
+    random_between(-180, 180)
+  )
+FROM generate_series(1, 50) AS loc_id;
+
+INSERT INTO dives (depth, dive_date, duration, diver_id, location_id)
+SELECT
+  random_between(1, 300) * RANDOM(),
+  NOW() - ('1 day'::INTERVAL*random_between(1, 1000)),
+  random_between(1, 720),
+  random_between(1, 5),
+  random_between(1, 50)
+FROM generate_series(1, 1000);
+
+COMMIT;
